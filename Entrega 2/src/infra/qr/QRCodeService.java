a5056e34b0a2398f7f78ec5e8010694d7e7c9a67
Juanes-gb <je.garciab123@uniandes.edu.co>
2025-12-06 23:41:12 -0500

    cambios para ultima entrega, implementacion ZXing

---
+++ Entrega 2/src/infra/qr/QRCodeService.java
@@ -0,0 +1,11 @@
+package infra.qr;
+
+//import java.awt.image.BufferedImage;
+
+public class QRCodeService {
+	//public BufferedImage generarQR(String contenido, int tamano) {
+		//return null;
+	//}
+}
+
+//FALTA IMPLEMENTAR CON LIBRERIA ZXing
---
+++ Entrega 2/src/infra/qr/QRException.java
@@ -0,0 +1,16 @@
+package infra.qr;
+
+public class QRException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public QRException(String mensaje) {
+        super(mensaje);
+    }
+
+    public QRException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
+    }
+}
+
+//FALTA IMPLEMENTAR
